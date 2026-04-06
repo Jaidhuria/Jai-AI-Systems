cd9e386d20c161958550938c1a2f0f77dceade4c
Jai Dhuria <jaidhuria002@gmail.com>
2026-04-06 20:42:10 +0530

    Update firebase.js

--- GlucoCare.AI/frontend/src/firebase.js
+++ GlucoCare.AI/frontend/src/firebase.js
@@ -3,14 +3,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyC8ucEOhI1kg15LHzbUHmP84i2LYdlLNJE",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "smartbus-8e09b.firebaseapp.com",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "smartbus-8e09b",
-  storageBucket:
-    process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "smartbus-8e09b.firebasestorage.app",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "967747934698",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:967747934698:web:073c3dd64315c0537a3a93",
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "G-RMC220BZ5Q",
+
 };
 
 const app = initializeApp(firebaseConfig);
